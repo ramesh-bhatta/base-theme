48d238e3ea4223ce551e9bf34a38d4fdca17ae3a
Sujan Prajapati Maharjan <prajapati_sujan1@hotmail.com>
2021-02-23 09:19:31 +1100

    Fix issue

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php 
 
-define("TECHIE_THEME_DIR",get_stylesheet_directory());
-define("TECHIE_THEME_URL",get_stylesheet_directory_uri());
+define("TECHIE_THEME_DIR",get_template_directory());
+define("TECHIE_THEME_URL",get_template_directory_uri());
 
 include TECHIE_THEME_DIR."/inc/init.php";
 
